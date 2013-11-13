00362080cfafaabf35a60b4409b4be678b468311
pbarchana <pb_archana@yahoo.com>
2013-11-13 15:58:17 -0800

    until adding 2 passes

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,20 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var qStart = 0;
+  var qEnd = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[qEnd++] = value;
   };
 
   instance.dequeue = function(){
   };
 
   instance.size = function(){
+    return qEnd - qStart;
   };
 
   return instance;
