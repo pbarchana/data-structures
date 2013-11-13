2d720ea0287f6a07045f660d608a24de2ac3c704
pbarchana <pb_archana@yahoo.com>
2013-11-13 15:51:45 -0800

    stack passes tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,28 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var length = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    length++;
+    storage[length] = value;
   };
 
   instance.pop = function(){
+    if (length <= 0) {
+      length = 0;
+      return undefined;
+    }
+    var item = storage[length];
+    delete storage[length];
+    length-- ;
+    return item;
   };
 
   instance.size = function(){
+   return length;
   };
 
   return instance;
