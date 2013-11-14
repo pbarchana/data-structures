dc8f39486d9c8d87fa09b06a0b7189648c2b2ee8
pbarchana <pb_archana@yahoo.com>
2013-11-13 16:29:11 -0800

    shared size works

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,39 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
+var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.length = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  instance.size = stackMethods.size;
+  
+  return instance;
 };
 
-var stackMethods = {};
+
+var stackMethods = { 
+/*	var push = function(value){
+    	length++;
+    	storage[length] = value;
+  	};
+
+	var pop = function(){
+		if (length <= 0) {
+		  length = 0;
+		  return undefined;
+		}
+		var item = storage[length];
+		delete storage[length];
+		length-- ;
+		return item;
+	};
+*/
+
+  	size : function(){
+   		console.log(this);
+   		return this.length;
+  	}
+
+};
